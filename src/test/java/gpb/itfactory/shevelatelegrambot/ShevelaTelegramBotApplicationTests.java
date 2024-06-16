3f5230cb3c2bbfaf93641a5ef856288687b12317
Ivan Shevela <ivan_shevela@mail.ru>
2024-06-16 14:58:17 +0300

    Задание 7,8. Обновлены зависимости, скорректирован gradle-build.yml (т.к. при создании PR и workflow gradle build упали тесты, хотя локально они проходят)

--- src/test/java/gpb/itfactory/shevelatelegrambot/ShevelaTelegramBotApplicationTests.java
+++ src/test/java/gpb/itfactory/shevelatelegrambot/ShevelaTelegramBotApplicationTests.java
@@ -3,7 +3,7 @@ package gpb.itfactory.shevelatelegrambot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ShevelaTelegramBotApplication.class)
 class ShevelaTelegramBotApplicationTests {
 
     @Test
